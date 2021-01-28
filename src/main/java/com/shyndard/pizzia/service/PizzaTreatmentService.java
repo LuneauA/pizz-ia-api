aa211e2541a0a732d5aacd609a3feb5b0d940bb7
Custo <shyndard@gmail.com>
2021-01-28 14:13:37 +0100

    Remove prediction

--- src/main/java/com/shyndard/pizzia/service/PizzaTreatmentService.java
+++ src/main/java/com/shyndard/pizzia/service/PizzaTreatmentService.java
@@ -11,20 +11,12 @@ import javax.inject.Inject;
 import javax.ws.rs.WebApplicationException;
 
 import com.shyndard.pizzia.entity.PizzaTreatment;
-import com.shyndard.pizzia.entity.dto.CallPredictionDto;
-import com.shyndard.pizzia.entity.dto.PredictionDto;
-
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @ApplicationScoped
 public class PizzaTreatmentService {
 
 	@Inject
 	S3StorageService storageService;
-
-	@Inject
-	@RestClient
-	PredictionService predictionService;
 
 	// TODO: Store in a database
 	private final List<PizzaTreatment> pizzas = new ArrayList<>();
@@ -61,13 +53,6 @@ public class PizzaTreatmentService {
 			pizza.setImageUrl(url.get());
 		} else {
 			throw new WebApplicationException("Cannot upload image to s3", 500);
-		}
-		// Call prediction API
-		PredictionDto prediction = predictionService.getPrediction(new CallPredictionDto(imgInBase64));
-		if(prediction == null) {
-			throw new WebApplicationException("Cannot get a valid prediction", 500);
-		} else {
-			pizza.setSuccess(prediction.getResult());
 		}
 		// TODO: Save prediction's result in database
 		return pizza;
